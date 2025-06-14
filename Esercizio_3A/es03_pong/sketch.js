7bf027790b37b95f4ed23071408c08ed90f2442e
greta <lopopologreta@gmail.com>
2025-06-14 17:09:30 +0200

    Update sketch.js

--- Esercizio_3A/es03_pong/sketch.js
+++ Esercizio_3A/es03_pong/sketch.js
@@ -6,11 +6,6 @@ let targetSize;
 
 let bgHue;
 
-let paddleWidth = 20;
-let paddleHeight = 100;
-let p1Y, p2Y;
-let paddleSpeed = 7;
-
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB, 360, 100, 100);
@@ -18,18 +13,14 @@ function setup() {
 
   posX = width / 2;
   posY = height / 2;
-  velX = random([-1, 1]) * random(5, 12);
+  velX = random([-1, 1]) * random(2, 22);
   velY = random([-1, 1]) * random(5, 12);
 
   ballColor = color(random(360), 80, 100);
-  targetSize = random(30, 70);
+  targetSize = random(10, 150); // inizio con range ampio
   ballSize = targetSize;
 
   bgHue = random(360);
-
-  // Posizione iniziale palette
-  p1Y = height / 2 - paddleHeight / 2;
-  p2Y = height / 2 - paddleHeight / 2;
 }
 
 function draw() {
@@ -47,80 +38,30 @@ function draw() {
   if (posY <= 0 || posY >= height) {
     velY *= -1;
     cambiaAspetto();
+    cambiaDimensione();
   }
 
-  // Collisione con paddle sinistra (Giocatore 1)
-  if (posX <= paddleWidth && posY > p1Y && posY < p1Y + paddleHeight) {
+  // Rimbalzo sui bordi orizzontali
+  if (posX <= 0 || posX >= width) {
     velX *= -1;
     cambiaAspetto();
-    posX = paddleWidth + 1;
+    cambiaDimensione();
   }
 
-  // Collisione con paddle destra (Giocatore 2)
-  if (posX >= width - paddleWidth && posY > p2Y && posY < p2Y + paddleHeight) {
-    velX *= -1;
-    cambiaAspetto();
-    posX = width - paddleWidth - 1;
-  }
-
-  // Punto perso (fuori dai bordi orizzontali)
-  if (posX < 0 || posX > width) {
-    resetBall();
-  }
-
-  // Animazione palla
+  // Anima dimensione palla verso targetSize
   ballSize += (targetSize - ballSize) * 0.2;
+
   fill(ballColor);
   noStroke();
   drawingContext.shadowBlur = 60;
   drawingContext.shadowColor = ballColor;
   ellipse(posX, posY, ballSize);
-
-  // Disegna paddles
-  drawPaddles();
-
-  // Movimento giocatori
-  movePlayers();
 }
 
 function cambiaAspetto() {
   ballColor = color(random(360), 80, 100);
-  targetSize = random(30, 70);
 }
 
-function resetBall() {
-  posX = width / 2;
-  posY = height / 2;
-  velX = random([-1, 1]) * random(5, 12);
-  velY = random([-1, 1]) * random(5, 12);
-  cambiaAspetto();
-}
-
-function drawPaddles() {
-  noStroke();
-  fill(0, 0, 100); // bianco
-  rect(0, p1Y, paddleWidth, paddleHeight); // Giocatore 1 (sinistra)
-  rect(width - paddleWidth, p2Y, paddleWidth, paddleHeight); // Giocatore 2 (destra)
-}
-
-function movePlayers() {
-  // Giocatore 1 (W/S)
-  if (keyIsDown(87)) { // W
-    p1Y -= paddleSpeed;
-  }
-  if (keyIsDown(83)) { // S
-    p1Y += paddleSpeed;
-  }
-
-  // Giocatore 2 (UP/DOWN)
-  if (keyIsDown(UP_ARROW)) {
-    p2Y -= paddleSpeed;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    p2Y += paddleSpeed;
-  }
-
-  // Limiti paddle
-  p1Y = constrain(p1Y, 0, height - paddleHeight);
-  p2Y = constrain(p2Y, 0, height - paddleHeight);
+function cambiaDimensione() {
+  targetSize = random(10, 150); // range maggiore per più differenza
 }
