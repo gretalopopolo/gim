f589285da7be1a6ecaa94e607c9ca1a435777442
greta <lopopologreta@gmail.com>
2025-06-14 15:49:58 +0200

    h

--- Esercizio_3A/es03_pong/sketch.js
+++ Esercizio_3A/es03_pong/sketch.js
@@ -4,56 +4,123 @@ let ballColor;
 let ballSize;
 let targetSize;
 
-let bgHue;  // Per il colore di sfondo che cambia velocemente
+let bgHue;
+
+let paddleWidth = 20;
+let paddleHeight = 100;
+let p1Y, p2Y;
+let paddleSpeed = 7;
 
 function setup() {
   createCanvas(800, 600);
   colorMode(HSB, 360, 100, 100);
   background(0);
 
-  posX = random(width);
-  velX = random(5, 12);
-  posY = random(height);
-  velY = random(5, 20);
+  posX = width / 2;
+  posY = height / 2;
+  velX = random([-1, 1]) * random(5, 12);
+  velY = random([-1, 1]) * random(5, 12);
 
   ballColor = color(random(360), 80, 100);
-  targetSize = random(30, 350);
+  targetSize = random(30, 70);
   ballSize = targetSize;
 
-  bgHue = random(360); // Inizializza il colore di sfondo
+  bgHue = random(360);
+
+  // Posizione iniziale palette
+  p1Y = height / 2 - paddleHeight / 2;
+  p2Y = height / 2 - paddleHeight / 2;
 }
 
 function draw() {
-  // Cambia il colore dello sfondo ad ogni frame (scorre lentamente)
-  bgHue = (bgHue + 1) % 360;  // Cambia il valore hue ad ogni frame (più veloce del cambio pallina)
-
+  // Sfondo animato
+  bgHue = (bgHue + 1) % 360;
   noStroke();
-  fill(bgHue, 80, 15, 0.05);  // Colore scuro ma con sfumatura e trasparenza per scia
+  fill(bgHue, 80, 15, 0.05);
   rect(0, 0, width, height);
 
+  // Muovi palla
   posX += velX;
   posY += velY;
 
-  if (posX >= width || posX <= 0) {
-    velX *= -1;
-    cambiaAspetto();
-  }
-
-  if (posY >= height || posY <= 0) {
+  // Rimbalzo su alto/basso
+  if (posY <= 0 || posY >= height) {
     velY *= -1;
     cambiaAspetto();
   }
 
-  ballSize += (targetSize - ballSize) * 0.2;
+  // Collisione con paddle sinistra (Giocatore 1)
+  if (posX <= paddleWidth && posY > p1Y && posY < p1Y + paddleHeight) {
+    velX *= -1;
+    cambiaAspetto();
+    posX = paddleWidth + 1;
+  }
 
+  // Collisione con paddle destra (Giocatore 2)
+  if (posX >= width - paddleWidth && posY > p2Y && posY < p2Y + paddleHeight) {
+    velX *= -1;
+    cambiaAspetto();
+    posX = width - paddleWidth - 1;
+  }
+
+  // Punto perso (fuori dai bordi orizzontali)
+  if (posX < 0 || posX > width) {
+    resetBall();
+  }
+
+  // Animazione palla
+  ballSize += (targetSize - ballSize) * 0.2;
   fill(ballColor);
   noStroke();
   drawingContext.shadowBlur = 60;
   drawingContext.shadowColor = ballColor;
   ellipse(posX, posY, ballSize);
+
+  // Disegna paddles
+  drawPaddles();
+
+  // Movimento giocatori
+  movePlayers();
 }
 
 function cambiaAspetto() {
   ballColor = color(random(360), 80, 100);
-  targetSize = random(30, 350);
+  targetSize = random(30, 70);
+}
+
+function resetBall() {
+  posX = width / 2;
+  posY = height / 2;
+  velX = random([-1, 1]) * random(5, 12);
+  velY = random([-1, 1]) * random(5, 12);
+  cambiaAspetto();
+}
+
+function drawPaddles() {
+  noStroke();
+  fill(0, 0, 100); // bianco
+  rect(0, p1Y, paddleWidth, paddleHeight); // Giocatore 1 (sinistra)
+  rect(width - paddleWidth, p2Y, paddleWidth, paddleHeight); // Giocatore 2 (destra)
+}
+
+function movePlayers() {
+  // Giocatore 1 (W/S)
+  if (keyIsDown(87)) { // W
+    p1Y -= paddleSpeed;
+  }
+  if (keyIsDown(83)) { // S
+    p1Y += paddleSpeed;
+  }
+
+  // Giocatore 2 (UP/DOWN)
+  if (keyIsDown(UP_ARROW)) {
+    p2Y -= paddleSpeed;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    p2Y += paddleSpeed;
+  }
+
+  // Limiti paddle
+  p1Y = constrain(p1Y, 0, height - paddleHeight);
+  p2Y = constrain(p2Y, 0, height - paddleHeight);
 }
---
+++ Esercizio_3A/es03_pong_interattivo/sketch.js
@@ -0,0 +1,133 @@
+let posX, velX;
+let posY, velY;
+let ballColor;
+let ballSize;
+let targetSize;
+
+let paddleWidth = 40;
+let paddleHeight = 200;
+let p1Y, p2Y;
+let paddleSpeed = 14;
+
+let score1 = 0;
+let score2 = 0;
+
+let trail = [];
+let maxTrailLength = 30;
+
+function setup() {
+  createCanvas(1600, 1200);
+  colorMode(HSB, 360, 100, 100, 1);
+  background(0);
+  textAlign(CENTER, TOP);
+  textSize(64);
+
+  posX = width / 2;
+  posY = height / 2;
+  velX = random([-1, 1]) * random(3, 20);
+  velY = random([-1, 1]) * random(3, 20);
+
+  ballColor = color(random(360), 80, 100);
+  targetSize = random(60, 140);
+  ballSize = targetSize;
+
+  p1Y = height / 2 - paddleHeight / 2;
+  p2Y = height / 2 - paddleHeight / 2;
+}
+
+function draw() {
+  background(0);
+
+  posX += velX;
+  posY += velY;
+
+  if (posY <= 0 || posY >= height) {
+    velY *= -1;
+    cambiaAspetto();
+  }
+
+  if (posX <= paddleWidth && posY > p1Y && posY < p1Y + paddleHeight) {
+    velX *= -1;
+    cambiaAspetto();
+    posX = paddleWidth + 1;
+  }
+
+  if (posX >= width - paddleWidth && posY > p2Y && posY < p2Y + paddleHeight) {
+    velX *= -1;
+    cambiaAspetto();
+    posX = width - paddleWidth - 1;
+  }
+
+  if (posX < 0) {
+    score2++;
+    resetBall();
+  } else if (posX > width) {
+    score1++;
+    resetBall();
+  }
+
+  trail.push({ x: posX, y: posY, size: ballSize, col: ballColor });
+
+  if (trail.length > maxTrailLength) {
+    trail.shift();
+  }
+
+  noStroke();
+  for (let i = 0; i < trail.length; i++) {
+    let alpha = map(i, 0, trail.length, 0, 0.7);
+    let size = map(i, 0, trail.length, trail[i].size * 0.5, trail[i].size);
+    fill(hue(trail[i].col), saturation(trail[i].col), brightness(trail[i].col), alpha);
+    ellipse(trail[i].x, trail[i].y, size);
+  }
+
+  ballSize += (targetSize - ballSize) * 0.2;
+  fill(ballColor);
+  noStroke();
+  drawingContext.shadowBlur = 120;
+  drawingContext.shadowColor = ballColor;
+  ellipse(posX, posY, ballSize);
+
+  drawPaddles();
+  movePlayers();
+  showScore();
+}
+
+function cambiaAspetto() {
+  ballColor = color(random(360), 80, 100);
+  targetSize = random(60, 140);
+}
+
+function resetBall() {
+  posX = width / 2;
+  posY = height / 2;
+  velX = random([-1, 1]) * random(10, 24);
+  velY = random([-1, 1]) * random(10, 24);
+  cambiaAspetto();
+  trail = [];
+}
+
+function drawPaddles() {
+  noStroke();
+  fill(0, 0, 100);
+  rect(0, p1Y, paddleWidth, paddleHeight);
+  rect(width - paddleWidth, p2Y, paddleWidth, paddleHeight);
+}
+
+function movePlayers() {
+  if (keyIsDown(87)) p1Y -= paddleSpeed;
+  if (keyIsDown(83)) p1Y += paddleSpeed;
+  if (keyIsDown(UP_ARROW)) p2Y -= paddleSpeed;
+  if (keyIsDown(DOWN_ARROW)) p2Y += paddleSpeed;
+
+  p1Y = constrain(p1Y, 0, height - paddleHeight);
+  p2Y = constrain(p2Y, 0, height - paddleHeight);
+}
+
+function showScore() {
+  fill(0, 0, 100);
+  noStroke();
+  textSize(64);
+  text(score1 + " : " + score2, width / 2, 40);
+  textSize(48);
+  text("PUNTEGGIO", width / 2, 110);
+}
